8a3e8aba5ed1f4dabbb0a5856b2c3891eeeabd35
leeyoungsub <liive29252@gmail.com>
2019-12-24 23:43:27 +0900

    single_layer

--- single_layer_Neural_Network.py
+++ single_layer_Neural_Network.py
@@ -1,14 +1,19 @@
 import numpy as np
-def sig(x):
-    y=1/(1+np.exp(-x))
-    y=np.where(y>=0.5,1,0)
-    return y
-x=np.array([[1,0,0,0]])
-d=np.array([[1,0]])
+import matplotlib.pyplot as plt
+x=np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+d=np.array([[1,0],[1,0],[0,1],[0,1]])
 w=np.random.rand(d.shape[1],x.shape[1])
-learning_rate=1e-4
-NET=np.dot(x,w.T)
-y=sig(NET)
-E=d-y
-
-print(y)
+learning_rate=1e-2
+N=400
+E_plot=np.zeros((N))
+for step in range(N):
+    NET=np.dot(x,w.T)
+    y=np.where(NET>=1,1,0)
+    E=(d-y)**2
+    dw=learning_rate*np.dot(E.T,x)
+    w+=dw
+    if step%100==0:
+        print(y)
+    E_plot[step]=E.sum()
+plt.plot(E_plot)
+plt.show()
